a2f67f83671c3bf71170d71fddc8de2cfa438d0c
karen <kfannin816@gmail.com>
2023-09-05 13:52:28 -0400

    lesson 17 complete

---
+++ src/app/photo-show/photo-show.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../photos.service';
+
+@Component({
+  selector: 'app-photo-show',
+  templateUrl: './photo-show.component.html',
+  styleUrls: ['./photo-show.component.css']
+})
+export class PhotoShowComponent implements OnInit {
+  photoUrl: string = "";
+
+  constructor(private photoService: PhotosService) { 
+  
+    this.fetchPhoto();
+
+  }
+  
+  onClick(){
+
+   this.fetchPhoto();
+  }
+  
+fetchPhoto(){
+  this.photoService.getPhoto().subscribe((response) => {
+    this.photoUrl = response.urls.regular;
+
+  });
+}
+
+  ngOnInit(): void {  }
+
+}
+
+ 
